f98384cd275af0e1b064cdad08eb42cad11c449e
TOBIN K TOMY <144670158+Tobztomy@users.noreply.github.com>
2024-02-29 15:24:22 +0530

    Add files via upload

---
+++ cycle 1/5.sh
@@ -0,0 +1,11 @@
+# Author : Tobin K Tomy
+echo Enter the string
+read s
+echo $s>temp
+rvs="$(rev temp)"
+if [ $s = $rvs ]
+then
+echo "it is palindrome"
+else
+echo "it is not a Palindrome"
+fi
---
+++ cycle 1/5n.sh
@@ -0,0 +1,12 @@
+# Author : Tobin K Tomy
+echo "Enter a string: "
+read s
+echo "$s" > temp
+rvs="$(rev < temp)"
+
+if [ "$s" = "$rvs" ]
+then
+echo "It is a palindrome"
+else
+echo "It is not a palindrome"
+fi
